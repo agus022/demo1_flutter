af85fee998ac796c27f375750a4725ecedc280ab
agus022 <flores.jorge.1j@gmail.com>
2025-04-01 18:58:12 -0600

    bck_detail_movie

--- lib/screens/detail_popular_screen.dart
+++ lib/screens/detail_popular_screen.dart
@@ -15,9 +15,44 @@ class _DetailPopularScreenState extends State<DetailPopularScreen> {
   Widget build(BuildContext context) {
 
     final popular = ModalRoute.of(context)!.settings.arguments as PopularModel;
+    const space = SizedBox(height: 10,);
     return Scaffold(
       //appBar: AppBar(title: Text(widget.popularModel!.title),),//la palabra widget
-      appBar: AppBar(title: Text(popular.title),),
+      appBar: AppBar(title: Text(popular.title),
+      ),
+      body: Hero(
+        tag: 'https://image.tmdb.org/t/p/w500/${popular.posterPath}',
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              opacity: .5,
+              image: NetworkImage('https://image.tmdb.org/t/p/w500/${popular.posterPath}'),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: ListView(
+            shrinkWrap: true,
+            children: [
+              Container(
+                height: 230,
+                color: Colors.red,
+              ),
+              space,
+              Container(
+                height: 200,
+                color: Colors.white12,
+                child: Text(popular.overview),
+              ),
+              space,
+              Container(
+                height: 120,
+                color: Colors.yellow,
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
